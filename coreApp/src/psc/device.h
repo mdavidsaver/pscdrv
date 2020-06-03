64e2fb4c607304d727b27c5b0f6ef9db10e9167d
Michael Davidsaver <mdavidsaver@gmail.com>
2020-06-03 16:19:15 -0700

    use shared_vector for buffer

--- coreApp/src/psc/device.h
+++ coreApp/src/psc/device.h
@@ -27,6 +27,8 @@
 #include <event2/bufferevent.h>
 #include <event2/buffer.h>
 
+#include <pv/sharedVector.h>
+
 #include "cblist.h"
 
 #if __cplusplus<201103L
@@ -78,7 +80,7 @@ struct Block
     PSCBase& psc;
     const epicsUInt16 code;
 
-    typedef std::vector<char> data_t;
+    typedef epics::pvData::shared_vector<const char> data_t;
     data_t data;
 
     bool queued;
